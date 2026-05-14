020881ef26e97935c9befc6ce033d6831af5812e
livanovic <livanovic@transperfect.com>
2026-05-15 00:00:46 +0200

    feat: add Todo model

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Todo todo1 = new Todo(1, "Kupiti mleko");
+        Todo todo2 = new Todo(2, "Nauciti Kafku");
+
+        System.out.println(todo1);
+        System.out.println(todo2);
+
+        todo1.setCompleted(true);
+        System.out.println(todo1);
     }
 }
---
+++ Todo.java
@@ -0,0 +1,24 @@
+public class Todo {
+
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public boolean isCompleted() { return completed; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
+
+    @Override
+    public String toString() {
+        return "[" + (completed ? "x" : " ") + "] " + id + ". " + title;
+    }
+}
